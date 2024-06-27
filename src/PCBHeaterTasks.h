2e0fce6ef64bb23d421fcbfd297607d6d9fcb0f6
Paul Carew <paul@clsreach.com>
2024-06-27 14:36:39 -0500

    Added documentation about task usage

--- src/PCBHeaterTasks.h
+++ src/PCBHeaterTasks.h
@@ -9,6 +9,14 @@
 #define PCBHEATERTASKS_H
 #include "pos/pos.h"
 
+/*
+ * Reason to have separate tasks:
+ * 	A task could be suspended, pending on some outside (of the task) event, thus not taking processing cycles
+ * 	A task could require different amount of stack space
+ * 	A task might have a different run schedule/priority
+ *
+ * 	Tasks may also invoke 'threads' in other classes. A Thread is NOT separately scheduled and is invoked from  within the a task
+ */
 class PCBHeaterTasks {
 public:
 	static void startup();
