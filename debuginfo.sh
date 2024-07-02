4b02986bc369d2830a28ee40cb47d7f65f1397ea
Paul Carew <paul@clsreach.com>
2024-07-02 14:17:06 -0500

    Symb Table now sorted

--- debuginfo.sh
+++ debuginfo.sh
@@ -1,2 +1,7 @@
-avr-objdump.exe -C -t *.elf > symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\*ABS\*"  > symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\.debug" |sort >> symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\.comment" |sort >> symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\.data" |sort >> symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\.bss" |sort >> symboltable.sym
+avr-objdump.exe -C -t *.elf |grep "\.text" |sort >> symboltable.sym
 avr-objdump.exe -d *.elf > dissassm.asm
