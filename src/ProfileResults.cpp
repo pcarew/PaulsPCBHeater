7783967c1892a19870d1e462706ea9903cad7d82
Paul Carew <paul@clsreach.com>
2024-07-06 15:21:48 -0500

    ProfileResults.cpp now outputs to Serial Plotter

--- src/ProfileResults.cpp
+++ src/ProfileResults.cpp
@@ -15,14 +15,13 @@ ProfileResults::~ProfileResults() {
 }
 
 void ProfileResults::tempDataPacket(unsigned long time,unsigned guard,unsigned guardCurrent, unsigned target,unsigned targetCurrent){
-
 // 	Output to serial for logging
-	char buff[30];
-	sprintf(buff,"%lu,%u,%u,%u,%u\n",time,guard,guardCurrent,target,targetCurrent);
+	char buff[20];
+	sprintf_P(buff,PSTR("%u,%u,%u,%u\n"),guard,guardCurrent,target,targetCurrent);
+//	Serial.println(strlen(buff));
 	Serial.print(buff);
 	// Save to EEPROM?
 
 
 	// Send to ProfileGrapher for display
-
 }
