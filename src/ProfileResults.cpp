55cc877f8e245fae862dde0ce23f5a11f3967320
Paul Carew <paul@clsreach.com>
2024-07-08 12:58:13 -0500

    Temporarily stopeed Serial output of results

--- src/ProfileResults.cpp
+++ src/ProfileResults.cpp
@@ -7,6 +7,7 @@
 
 #include <arduino.h>
 #include "ProfileResults.h"
+#include "ProfileGrapher.h"
 
 ProfileResults::ProfileResults() {
 }
@@ -16,12 +17,13 @@ ProfileResults::~ProfileResults() {
 
 void ProfileResults::tempDataPacket(unsigned long time,unsigned guard,unsigned guardCurrent, unsigned target,unsigned targetCurrent){
 // 	Output to serial for logging
-	char buff[20];
-	sprintf_P(buff,PSTR("%u,%u,%u,%u\n"),guard,guardCurrent,target,targetCurrent);
+//	char buff[20];
+//	sprintf_P(buff,PSTR("%u,%u,%u,%u\n"),guard,guardCurrent,target,targetCurrent);
 //	Serial.println(strlen(buff));
-	Serial.print(buff);
+//	Serial.print(buff);
 	// Save to EEPROM?
 
 
 	// Send to ProfileGrapher for display
+	ProfileGrapher::tempDataPacket(time, guardCurrent, targetCurrent);
 }
